c6f3cd1a0efc1add275eea9b39e952ae0b421738
lkraem <lisa.kraemers@gmail.com>
2022-08-02 14:55:25 +0200

    very basic styling

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,14 @@ import SideBar from "./components/SideBar";
 export default function App() {
   return (
     <div className="App">
-      <SideBar />
-      <Outlet />
+      <div className="flex flex-row top-0 left-0 w-screen h-screen">
+        <div className="flex flex-col top-0 left-0 w-1/6 h-screen bg-red-600">
+          <SideBar />
+        </div>
+        <div className="flex flex-row top-0 left-0 w-5/6 h-screen bg-teal-300">
+          <Outlet />
+        </div>
+      </div>
     </div>
   );
 }
