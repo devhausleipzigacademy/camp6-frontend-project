b5e9f679552be2de23a105ffc54248379b1546fb
0101110101 <51328751+0101110101@users.noreply.github.com>
2022-08-03 12:54:13 +0200

    first commit,

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import { Outlet } from "react-router-dom";
+import { Header } from "../src/components/Header"
 import SideBar from "./components/SideBar";
 
 export default function App() {
   return (
     <div className="App">
-      <div className="flex flex-row top-0 left-0 w-screen h-screen">
-        <div className="flex flex-col top-0 left-0 w-1/6 h-screen bg-red-600">
+<Header/>
+      <div className="flex  h-[100vh]  flex-row  ">
+
+        <div className=" w-[25vw] h-full bg-slate-100">
           <SideBar />
         </div>
-        <div className="flex flex-row top-0 left-0 w-5/6 h-screen bg-teal-300">
+        <div className="flex flex-row w-full  shadow-md shadow-black bg-teal-300">
           <Outlet />
         </div>
       </div>
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,13 +1,17 @@
-import { NavLink } from "react-router-dom";
+import {NavLink} from "react-router-dom";
 
 export default function SideBar() {
-  return (
-    <div className="flex flex-col">
-      <NavLink to="/"> Home</NavLink>
-      <NavLink to="/tracks">Tracks</NavLink>
-      <NavLink to="/topics">Topics</NavLink>
-      <NavLink to="/feed">Feed</NavLink>
-      <NavLink to="/resources">Resources</NavLink>
-    </div>
-  );
+	return (
+		<div className="flex border-r-2 justify-between border-slate-300 h-full flex-col ml-2 ">
+			<div className="flex  flex-col text-start font-subheading font-bold gap-y-5 text-xl   h-full bg-slate-100  pt-28">
+				<NavLink to="/"> Home</NavLink>
+				<NavLink to="/tracks">Tracks</NavLink>
+				<NavLink to="/topics">Topics</NavLink>
+				<NavLink to="/feed">Feed</NavLink>
+				<NavLink to="/resources">Resources</NavLink>
+			</div>
+			<div className="h-20 w-64 self-center mt-4 bg-cyan-600">
+      TIMER</div>
+		</div>
+	);
 }
