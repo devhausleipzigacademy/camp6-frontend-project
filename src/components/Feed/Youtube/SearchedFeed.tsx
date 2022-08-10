0523c5d085e125e0901909f70c4dd627edbbd56b
0101110101 <51328751+0101110101@users.noreply.github.com>
2022-08-11 00:15:49 +0200

    fam innit

--- src/components/Feed/Youtube/SearchedFeed.tsx
+++ src/components/Feed/Youtube/SearchedFeed.tsx
@@ -9,7 +9,7 @@ import { moveCursor } from "readline";
 export default function SearchedFeed() {
 	const [articles, setArticles] = useState([]);
 	const [search, setSearch] = useState("");
-	const [query, setQuery] = useState("food");
+	const [query, setQuery] = useState("Greece");
 	const[limit,setLimit]=useState('3')
 
 	useEffect(() => {
@@ -80,24 +80,29 @@ return (
 				</form>
 			</div>
 
-			<div className="flex grow relative flex-col pl-10 w-full mt-7 no-scrollbar items-center justify-evenly  gap-14">
-
-	{articles.filter((x)=>x.data.is_video ||x.data.url).map((x)=>(
-
-
-	<div className="relative">
-<svg xmlns="http://www.w3.org/2000/svg" className="h-6 hover:fill-green-400 active:fill-purple-500 right-12 top-5 z-10 absolute w-6" fill="gray" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-  <path stroke-linecap="round" stroke-linejoin="round" d="M5 5a2 2 0 012-2h10a2 2 0 012 2v16l-7-3.5L5 21V5z" />
+			<div className="flex  relative flex-col pl-10 h-full w-full mt-20 no-scrollbar items-center justify-evenly  gap-20">
+			{articles
+        .filter((post) => !post.data.is_gallery)
+        .map((post) => (
+          <>
+            {post.data.is_video && !post.data.media.is_gif ? (<div className="flex flex-1 flex-col h-full w-full items-center">
+<svg xmlns="http://www.w3.org/2000/svg" className="h-6 hover:fill-green active:fill-purple w-6" viewBox="0 0 20 20" fill="currentColor">
+  <path d="M5 4a2 2 0 012-2h6a2 2 0 012 2v14l-5-2.5L5 18V4z" />
 </svg>
-	<iframe className="border-2 relative hover:scale-[100.8%] duration-200 rounded-xl flex  border-black border-opacity-25   shadow-inner shadow-black " id="reddit-embed embed-responsive"  src={`https://www.redditmedia.com/${x.data.permalink}?ref_source=embed&amp;ref=share&amp;embed=true;&autoplay=false` } sandbox="allow-scripts allow-orientation-lock allow-same-origin"  height="600" width="900"   do-not-allow="autoplay" scrolling="no">
-
-
-
-
-	</iframe>
-
-</div>]
-	))}</div>
+              <video className="rounded-lg h-[50rem] "
+                controls
+                src={post.data.media.reddit_video.fallback_url}
+              />	<a href={post.data.url}>here the link to go</a></div>
+            ) : (
+<div>
+	<svg xmlns="http://www.w3.org/2000/svg" className="h-6 hover:fill-green z-10 active:fill-purple w-6" viewBox="0 0 20 20" fill="currentColor">
+  <path d="M5 4a2 2 0 012-2h6a2 2 0 012 2v14l-5-2.5L5 18V4z" />
+</svg><iframe className="border-2 relative hover:scale-[100.8%] duration-200 rounded-xl flex  border-black border-opacity-25   shadow-inner shadow-black " id="reddit-embed embed-responsive"  src={`https://www.redditmedia.com/${post.data.permalink}?ref_source=embed&amp;ref=share&amp;embed=true;&autoplay=false` } sandbox="allow-scripts allow-orientation-lock allow-same-origin"  height="500" width="900"   do-not-allow="autoplay" scrolling="no"></iframe>
+</div>
+            )}
+          </>
+        ))}
+	</div>
 
 
 
@@ -107,17 +112,24 @@ return (
 
 
 
-// this is safecode let it stay here for while. I dont trust iframe at all and  I dont want to lost the code below and being have write it again
-{/* <div>	{articles
-					.filter((post) => !post.data.is_gallery && !(post.data.post_hint==="link"?true:false) &&post.data.post_hint &&!(post.data.post_hint==="self"?true:false)&&!(post.data.domain==="i.imgur.com"?true:false)&&!(post.data.type==="i.imgur.com"?true:false))
-					.flatMap((post) => (<>
-					<div className="text-center flex flex-col  p-4 border-opacity-25  scrollbar-hide w-[60%] border-black shadow-black rounded-xl border-2  justify-center  bg-purple-200 ">
-					<svg xmlns="http://www.w3.org/2000/svg" className="h-6 self-end hover:fill-green-600 w-6" fill='' viewBox="0 0 24 24" stroke="" stroke-width="2">
-  <path stroke-linecap="round" stroke-linejoin="round" d="M5 5a2 2 0 012-2h10a2 2 0 012 2v16l-7-3.5L5 21V5z" />
-</svg>
-							<h1>{post.data.title}</h1>
-						{
-							post.data.is_video && !post.data.media.is_gif ? <figure className="h-[20vh] w-[max] "><iframe className="h-full w-full" id="reddit-embed" src={`https://www.redditmedia.com/${post.data.permalink}?ref_source=embed&amp;ref=share&amp;embed=true` } sandbox="allow-scripts allow-same-origin allow-popups"  height="full]" width="full" scrolling="no"></iframe></figure> : <img src={post.data.url_overridden_by_dest} />}
-						</div>
-						</>
-					))}</div> */}
+
+
+
+
+
+
+
+// 	<div className="relative">
+// <svg xmlns="http://www.w3.org/2000/svg" className="h-6 hover:fill-green-400 active:fill-purple-500 right-12 top-5 z-10 absolute w-6" fill="gray" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+//   <path stroke-linecap="round" stroke-linejoin="round" d="M5 5a2 2 0 012-2h10a2 2 0 012 2v16l-7-3.5L5 21V5z" />
+// </svg>
+// 	<iframe className="border-2 relative hover:scale-[100.8%] duration-200 rounded-xl flex  border-black border-opacity-25   shadow-inner shadow-black " id="reddit-embed embed-responsive"  src={`https://www.redditmedia.com/${x.data.permalink}?ref_source=embed&amp;ref=share&amp;embed=true;&autoplay=false` } sandbox="allow-scripts allow-orientation-lock allow-same-origin"  height="600" width="900"   do-not-allow="autoplay" scrolling="no"></iframe>
+
+
+
+
+//
+
+// </div>
+//))}
+
