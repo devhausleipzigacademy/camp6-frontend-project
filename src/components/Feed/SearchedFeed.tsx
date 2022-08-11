14ada6ccc6075561772dd88b5e0cca60bb9f02d6
0101110101 <51328751+0101110101@users.noreply.github.com>
2022-08-11 23:45:31 +0200

    sry forgot smth

--- src/components/Feed/SearchedFeed.tsx
+++ src/components/Feed/SearchedFeed.tsx
@@ -134,7 +134,7 @@ export default function SearchedFeed({ task }: any) {
 								/>
 								<p>{post.data.selftext}</p>
 							<a href={redditUrl + post.data.permalink}>	<p className="text-center hover:scale-[105%] hover:underline mt-2">{post.data.num_comments} <br/> comments </p></a>
-								<svg xmlns="http://www.w3.org/2000/svg" className="h-5 self-center hover:scale-[110%] w-5" viewBox="0 0 20 20" fill="currentColor">
+								<svg xmlns="http://www.w3.org/2000/svg" className="h-5 self-center  w-5" viewBox="0 0 20 20" fill="currentColor">
   <path fill-rule="evenodd" d="M18 13V5a2 2 0 00-2-2H4a2 2 0 00-2 2v8a2 2 0 002 2h3l3 3 3-3h3a2 2 0 002-2zM5 7a1 1 0 011-1h8a1 1 0 110 2H6a1 1 0 01-1-1zm1 3a1 1 0 100 2h3a1 1 0 100-2H6z" clip-rule="evenodd" />
 </svg>
 							</div>
