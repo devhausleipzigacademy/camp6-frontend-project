8f938376f8d2db288dae2c4031406ea0e50d0e56
0101110101 <51328751+0101110101@users.noreply.github.com>
2022-08-11 09:14:59 +0200

    before merge

--- src/routes/Feed.tsx
+++ src/routes/Feed.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 
 import { data } from "autoprefixer";
 
-import SearchedFeed from "../components/Feed/Youtube/SearchedFeed";
+import SearchedFeed from "../components/Feed/SearchedFeed";
 import { Youtube } from "../components/Feed/Youtube/YoutubeConfig";
 
 
