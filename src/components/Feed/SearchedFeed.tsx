d02cc41613538d7d7e719a17a4a74c514dfee571
0101110101 <51328751+0101110101@users.noreply.github.com>
2022-08-11 21:21:08 +0200

    reddit

--- src/components/Feed/SearchedFeed.tsx
+++ src/components/Feed/SearchedFeed.tsx
@@ -5,13 +5,13 @@ import { Searchbar } from "./SearchBar";
 import { setInterval } from "timers/promises";
 import { Youtube } from "./Youtube/YoutubeConfig";
 import { moveCursor } from "readline";
-import { taskDummy, tasksDummies } from "../../database/Dummies";
+import { taskDummy, tasksDummies, tracksDummies } from "../../database/Dummies";
 
 export default function SearchedFeed({ task }: any) {
 	const [articles, setArticles] = useState([]);
 	const [search, setSearch] = useState("");
 	const [query, setQuery] = useState("news");
-	const [limit, setLimit] = useState("5");
+	const [limit, setLimit] = useState("14");
 	const [save, setSave] = useState("");
 
 	useEffect(() => {
@@ -44,30 +44,34 @@ export default function SearchedFeed({ task }: any) {
 		setQuery(search);
 	}
 
-	let display = "";
+	console.log(query);
 
 	function dropDown() {
 		const drop = document.getElementById("dropDown");
 	}
-
+	let redditUrl = "https://www.reddit.com/";
 	const [opacity, setOpacity] = useState("100");
 	return (
 		<div className=" ">
 			<Youtube />
-			<div>
-				<p>{taskDummy.name}</p>
-			</div>
 
-			<div className="flex flex-row mt-10 items-end justify-end">
-				<div className="inline-block  hidden relative w-64">
-					<select className=" appearance-none w-full  px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
-						<p>tasks</p>
-						<option> 2</option>
-						<option> 3</option>
+			<div className="flex flex-row mt-10 items-end justify-between">
+				<div className="inline-block   relative w-64">
+					<p>Filter by Tracks</p>
+					<select
+						onChange={(e) => {
+							setQuery(e.target.value);
+						}}
+						className=" appearance-none w-full  hover:bg-purple-200 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
+					>
+						<option value="">Select Track</option>
+						{tracksDummies.map((track, ix) => (
+							<option value={tracksDummies[ix]}>{tracksDummies[ix]}</option>
+						))}
 					</select>
-					<div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+					<div className="pointer-events-none absolute  inset-y-0 right-0 flex items-center px-2 text-gray-700">
 						<svg
-							className="fill-black text-black h- w-4"
+							className="fill-black text-black text mt-6 h-6 w-9"
 							xmlns="http://www.w3.org/2000/svg"
 							viewBox="0 0 20 20"
 						>
@@ -96,62 +100,32 @@ export default function SearchedFeed({ task }: any) {
 
 			<div className="flex  relative flex-col  pl-10 h-full w-full mt-20 no-scrollbar items-center justify-evenly  gap-20">
 				{articles
-					.filter((post) => !post.data.is_gallery)
+					.filter((post) =>
+						!post.data.is_gallery &&
+						post.data.domain === "i.imgur.com" &&
+						post.data.domain === "i.redd.it"
+							? false
+							: true && !post.data.is_video
+					)
 					.map((post) => (
 						<>
-							{post.data.is_video && !post.data.media.is_gif ? (
-								<div className="flex flex-1  hover:scale-[10px] flex-col h-56 bg-gray-300 bg-opacity-40 rounded-xl w-fit items-center">
-									<svg
-										type="button"
-										xmlns="http://www.w3.org/2000/svg"
-										onClick={colorChange}
-										className={`h-6 ${color} duration-200 hover:fill-green-500 self-end  active:fill-purple-400 w-6`}
-										viewBox="0 0 20 20"
-										fill={color}
-									>
-										<path d="M5 4a2 2 0 012-2h6a2 2 0 012 2v14l-5-2.5L5 18V4z" />
-									</svg>
-									<video
-										className="rounded-lg h-[calc(100vh-20rem)]   w-full "
-										controls
-										src={post.data.media.reddit_video.fallback_url}
-									/>{" "}
-									<p className="my-2 text-lg border-b-2 mx-1 ">
-										{post.data.title}
-									</p>
-									<a href={post.data.url}>
-										<button className="h-fit w-fit bg bg-purple-400 rounded-md border-2 font-subheading font-light border-opacity-40 hover:border-2 hover:border-green-400 p-2 my-2  scale-100 border-black px-1">
-											{" "}
-											Take Me To The Link
-										</button>
-									</a>
-								</div>
-							) : (
-								<div className="flex flex-1    h-[calc(100vh-15rem)] flex-row grow ">
-									<div className="flex flex-col   relative ">
-										<svg
-											id="button1"
-											onClick={colorChange}
-											xmlns="http://www.w3.org/2000/svg"
-											className="h-6  hover:scale-[100.8%] duration-200 z-10 top-5 left15  place-self-end  hover:fill-green-400  active:fill-purple-400 w-6"
-											viewBox="0 0 20 20"
-											fill={color}
-										>
-											<path d="M5 4a2 2 0 012-2h6a2 2 0 012 2v14l-5-2.5L5 18V4z" />
-										</svg>
-										<iframe
-											className="border-2    relative  rounded-2xl flex   "
-											id="reddit-embed embed-responsive"
-											src={`https://www.redditmedia.com/${post.data.permalink}?ref_source=embed&amp;ref=share&amp;embed=true;&autoplay=false`}
-											sandbox="allow-scripts allow-orientation-lock allow-same-origin"
-											height="520"
-											width="900"
-											do-not-allow="autoplay"
-											scrolling="no"
-										></iframe>
-									</div>
-								</div>
-							)}
+							<div className="h-full w-fit p-10 flex text-sm    rounded-2xl relative text-black flex-col  bg-white">
+								<p className="font-medium ">{post.data.title}</p>
+								<p>{post.data.selftext}</p>
+								<a href={redditUrl + post.data.permalink}>
+									<img
+										className="h-6 hover:scale-[105%] absolute right-3 top-2"
+										src="https://logodownload.org/wp-content/uploads/2018/02/reddit-logo-16.png"
+										alt=""
+									/>
+								</a>
+								<img
+									className="h-[25rem] w-fit self-center    rounded-2xl p-4 "
+									src={post.data.url}
+									alt=""
+								/>
+								<p>{post.data.selftext}</p>
+							</div>
 						</>
 					))}
 			</div>
@@ -169,3 +143,16 @@ export default function SearchedFeed({ task }: any) {
 
 // </div>
 //))}
+
+{
+	/* <iframe
+className="border-2    relative  rounded-2xl flex   "
+id="reddit-embed embed-responsive"
+src={`https://www.redditmedia.com/${post.data.permalink}?ref_source=embed&amp;ref=share&amp;embed=true;&autoplay=false`}
+sandbox="allow-scripts allow-orientation-lock allow-same-origin"
+height="520"
+width="900"
+do-not-allow="autoplay"
+scrolling="no"
+></iframe> */
+}
