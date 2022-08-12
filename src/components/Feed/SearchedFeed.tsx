9d8ae58b06d63fb054869f92354769eef77f153d
0101110101 <51328751+0101110101@users.noreply.github.com>
2022-08-12 14:15:48 +0200

    the closest version of the feed to figma blueprint

--- src/components/Feed/SearchedFeed.tsx
+++ src/components/Feed/SearchedFeed.tsx
@@ -6,26 +6,61 @@ import { setInterval } from "timers/promises";
 import { Youtube } from "./Youtube/YoutubeConfig";
 import { moveCursor } from "readline";
 import { taskDummy, tasksDummies, tracksDummies } from "../../database/Dummies";
+import { Console } from "console";
 
 export default function SearchedFeed({ task }: any) {
-	const [articles, setArticles] = useState([]);
+	const [articles, setArticles] = useState([] as Array);
 	const [search, setSearch] = useState("");
-	const [query, setQuery] = useState("photography");
+	const [query, setQuery] = useState("cat");
 	const [limit, setLimit] = useState("100");
 	const [save, setSave] = useState("");
-
+	const [isHidden, setIsHidden] = useState(true);
+	const [opacity, setOpacity] = useState(0.1);
+	function randomize(arr: Array<any>): Array<any> {
+		var i, j, tmp;
+		for (i = arr.length - 1; i > 0; i--) {
+			j = Math.floor(Math.random() * (i + 1));
+			tmp = arr[i];
+			arr[i] = arr[j];
+			arr[j] = tmp;
+		}
+		console.log(arr);
+		return arr;
+	}
 	useEffect(() => {
-		fetch(`https://www.reddit.com/search.json?limit=${limit}&q=${query}&top
-		`).then((result) => {
-			if (result.status != 200) {
-				console.log("ERRPORR, BRUVVV");
-			}
-			result.json().then((datas) => {
-				if (datas != null) {
-					setArticles(datas.data.children);
-				}
+		console.log(articles);
+	}, [articles]);
+	useEffect(() => {
+		if (query === "Random") {
+			Promise.all(
+				tracksDummies.map((item, idx) =>
+					fetch(`https://www.reddit.com/search.json?limit=20&q=${item}&top`)
+				)
+			).then((listOfResponses) => {
+				Promise.all(
+					listOfResponses.map(async (response) => {
+						console.log(response);
+
+						return await response.json().then((responseObj) => {
+							return responseObj.data.children;
+						});
+					})
+				).then((posts) => setArticles(randomize(posts.flat())));
 			});
-		});
+		} else {
+			fetch(`https://www.reddit.com/search.json?limit=${limit}&q=${query}&top
+		`).then((result) => {
+				if (result.status != 200) {
+					console.log("ERRPORR, BRUVVV");
+				}
+
+				result.json().then((datas) => {
+					if (datas != null) {
+						setArticles(datas.data.children);
+					}
+				});
+			});
+		}
 	}, [query]);
 
 	let color = "";
@@ -44,33 +79,16 @@ export default function SearchedFeed({ task }: any) {
 		setQuery(search);
 	}
 
-const[popUp,setPopUp]=useState("hidden opacity-0")
-
-
-function popUps(){
-	setPopUp("opacity-0 flex")
-
-}
-
-
-
-
-
-
-
-
 	console.log(query);
 
 	function dropDown() {
 		const drop = document.getElementById("dropDown");
 	}
 	let redditUrl = "https://www.reddit.com/";
-	const [opacity, setOpacity] = useState("100");
+
 	return (
 		<div className=" ">
 			<Youtube />
-
-
 
 			<div className="flex flex-row mt-10 items-end justify-between">
 				<div className="inline-block   relative w-64">
@@ -79,9 +97,10 @@ function popUps(){
 						onChange={(e) => {
 							setQuery(e.target.value);
 						}}
-						className=" appearance-none w-full  hover:bg-purple-200 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
+						className=" appearance-none w-full    px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
 					>
 						<option value="">Select Track</option>
+						<option value="Random">Random</option>
 						{tracksDummies.map((track, ix) => (
 							<option value={tracksDummies[ix]}>{tracksDummies[ix]}</option>
 						))}
@@ -117,36 +136,49 @@ function popUps(){
 
 			<div className="flex  relative flex-col  pl-10 h-full w-full mt-20 no-scrollbar items-center justify-evenly  gap-20">
 				{articles
-					.filter((post) =>
-					post.data.post_hint==='image'?true:false
-
-
-					)
+					.filter((post) => (post.data.post_hint === "image" ? true : false))
 					.map((post) => (
 						<>
-							<div className="h-full w-fit p-10 flex text-sm    rounded-2xl relative text-black flex-col  bg-white">
+							<div className="h-full w-fit p-10 flex text-sm   border-2 border-black border-opacity-40 rounded-2xl relative text-black flex-col  bg-white">
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									className="h-6 absolute bg  hover:fill-green-500 active:fill-purple-500 -top-6 right-2 w -6"
+									fill="currentColor"
+									viewBox="0 0 24 24"
+									stroke="currentColor"
+									stroke-width="1"
+								>
+									<path
+										stroke-linecap="round"
+										stroke-linejoin="round"
+										d="M5 5a2 2 0 012-2h10a2 2 0 012 2v16l-7-3.5L5 21V5z"
+									/>
+								</svg>
 
-<button onClick={popUps}>
-	<svg    xmlns="http://www.w3.org/2000/svg" className="h-6 absolute bg  hover:fill-green-500 active:fill-purple-500 -top-6 right-2 w -6" fill="currentColor" viewBox="0 0 24 24" stroke="currentColor" stroke-width="1">
-			<path stroke-linecap="round" stroke-linejoin="round" d="M5 5a2 2 0 012-2h10a2 2 0 012 2v16l-7-3.5L5 21V5z" />
-	</svg>
-</button>
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									className="h-6 absolute -left-2 -top-7 w-6"
+									fill="none"
+									viewBox="0 0 24 24"
+									stroke="currentColor"
+									stroke-width="2"
+								>
+									<path
+										stroke-linecap="round"
+										stroke-linejoin="round"
+										d="M7 11l5-5m0 0l5 5m-5-5v12"
+									/>
+								</svg>
 
-	<div id="popUp"   className={`   flex-col ${popUp} duration-1000  absolute -top-3 -right-32 rounded-md w-32 bg-black`}>
-			{tracksDummies.map((x,idx)=>(
-<p  className="text-white rounded-md active:text-[15px] hover:scale-[105%] active:text-yellow-300 border-b-2 p-2">{tracksDummies[idx]}</p>
-
-
-			))}
-			</div>
-
-<svg xmlns="http://www.w3.org/2000/svg" className="h-6 absolute -left-2 -top-7 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-  <path stroke-linecap="round" stroke-linejoin="round" d="M7 11l5-5m0 0l5 5m-5-5v12" />
-</svg>
-
-<p className="absolute text-xsm  -top-6  left-4">{post.data.ups}  times Up voted </p>
-							<p className="text-[9px] absolute right-10 -top-4">{post.data.subreddit_name_prefixed}</p>
-							<p className="  absolute top-1 text-[10px]">posted by u/{post.data.author}</p>
+								<p className="absolute text-xsm  -top-6  left-4">
+									{post.data.ups} times p voted{" "}
+								</p>
+								<p className="text-[9px] absolute right-10 -top-4">
+									{post.data.subreddit_name_prefixed}
+								</p>
+								<p className="  absolute top-1 text-[10px]">
+									posted by u/{post.data.author}
+								</p>
 								<p className="font-medium ">{post.data.title}</p>
 								<p>{post.data.selftext}</p>
 								<a href={redditUrl + post.data.permalink}>
@@ -162,10 +194,24 @@ function popUps(){
 									alt=""
 								/>
 								<p>{post.data.selftext}</p>
-							<a href={redditUrl + post.data.permalink}>	<p className="text-center hover:scale-[105%] hover:underline mt-2">{post.data.num_comments} <br/> comments </p></a>
-								<svg xmlns="http://www.w3.org/2000/svg" className="h-5 self-center  w-5" viewBox="0 0 20 20" fill="currentColor">
-  <path fill-rule="evenodd" d="M18 13V5a2 2 0 00-2-2H4a2 2 0 00-2 2v8a2 2 0 002 2h3l3 3 3-3h3a2 2 0 002-2zM5 7a1 1 0 011-1h8a1 1 0 110 2H6a1 1 0 01-1-1zm1 3a1 1 0 100 2h3a1 1 0 100-2H6z" clip-rule="evenodd" />
-</svg>
+								<a href={redditUrl + post.data.permalink}>
+									{" "}
+									<p className="text-center hover:scale-[105%] hover:underline mt-2">
+										{post.data.num_comments} <br /> comments{" "}
+									</p>
+								</a>
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									className="h-5 self-center  w-5"
+									viewBox="0 0 20 20"
+									fill="currentColor"
+								>
+									<path
+										fill-rule="evenodd"
+										d="M18 13V5a2 2 0 00-2-2H4a2 2 0 00-2 2v8a2 2 0 002 2h3l3 3 3-3h3a2 2 0 002-2zM5 7a1 1 0 011-1h8a1 1 0 110 2H6a1 1 0 01-1-1zm1 3a1 1 0 100 2h3a1 1 0 100-2H6z"
+										clip-rule="evenodd"
+									/>
+								</svg>
 							</div>
 						</>
 					))}
