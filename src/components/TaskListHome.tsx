9921ef623e1084a7c9fe590b689f0dcc54033da9
0101110101 <51328751+0101110101@users.noreply.github.com>
2022-08-19 10:42:05 +0200

    useTask has been uptaded to custom hooked and

--- src/components/TaskListHome.tsx
+++ src/components/TaskListHome.tsx
@@ -1,67 +1,68 @@
 import { CreateContext, Task, TaskProp } from "../types/TypesNConsts";
+import { useTasks } from "../utilities/axios";
 import { StarButton } from "./buttons/StarButton";
-import { ACTIONS, useTasks, useTasksDispatch } from "./TasksContext";
+import { ACTIONS, useTasksDispatch } from "./TasksContext";
 
 export function TaskListHome() {
-  const tasks = useTasks();
+	const tasks = useTasks();
 
-  if (!tasks.length) {
-    return <p>Oops, looks like you haven't added anything yet</p>;
-  }
+	if (!tasks.length) {
+		return <p>Oops, looks like you haven't added anything yet</p>;
+	}
 
-  return (
-    <div>
-      <h3 className="card-heading">Upcoming Tasks</h3>
-      <ul className="px-4 py-2">
-        {tasks
-          .sort(
-            (a: Task, b: Task) => a.deadline.getTime() - b.deadline.getTime()
-          )
-          .filter((task) => !task.completed ?? false)
-          .splice(0, 5)
-          .map((task) => (
-            <li
-              key={task.id}
-              className="flex p-3 border-r border-b rounded-md  justify-between items-center gap-2 h-9 bg-white"
-            >
-              <TaskItem task={task} />
-            </li>
-          ))}
-      </ul>
-    </div>
-  );
+	return (
+		<div>
+			<h3 className="card-heading">Upcoming Tasks</h3>
+			<ul className="px-4 py-2">
+				{tasks
+					.sort(
+						(a: Task, b: Task) => a.deadline.getTime() - b.deadline.getTime()
+					)
+					.filter((task) => !task.completed ?? false)
+					.splice(0, 5)
+					.map((task) => (
+						<li
+							key={task.id}
+							className="flex h-9 items-center justify-between gap-2  rounded-md border-r border-b bg-white p-3"
+						>
+							<TaskItem task={task} />
+						</li>
+					))}
+			</ul>
+		</div>
+	);
 }
 
 function TaskItem({ task }: TaskProp) {
-  // I will probably need these here
-  const dispatch = useTasksDispatch() as CreateContext;
+	// I will probably need these here
+	const dispatch = useTasksDispatch() as CreateContext;
 
-  const date = JSON.stringify(task.deadline).slice(1, 11);
+	const date = JSON.stringify(task.deadline).slice(1, 11);
 
-  return (
-    <>
-      <div className="flex flex-row w-full h-full gap-5 items-center">
-        <input
-          type="checkbox"
-          name="completed"
-          onChange={() => {
-            dispatch({ type: ACTIONS.COMPLETED, payload: { id: task.id } });
-          }}
-        />
+	return (
+		<>
+			<div className="flex h-full w-full flex-row items-center gap-5">
+				<input
+					type="checkbox"
+					name="completed"
+					onChange={() => {
+						dispatch({ type: ACTIONS.COMPLETED, payload: { id: task.id } });
+					}}
+				/>
 
-        <p className="text-xs">{task.name}</p>
+				<p className="text-xs">{task.name}</p>
 
-        <p className="text-xs text-customTextColorLight ">Due: {date}</p>
-      </div>
-      <StarButton
-        value={task.priority}
-        clickHandler={() => {
-          dispatch({
-            type: ACTIONS.PRIORITY,
-            payload: { id: task.id },
-          });
-        }}
-      />
-    </>
-  );
+				<p className="text-xs text-customTextColorLight ">Due: {date}</p>
+			</div>
+			<StarButton
+				value={task.priority}
+				clickHandler={() => {
+					dispatch({
+						type: ACTIONS.PRIORITY,
+						payload: { id: task.id },
+					});
+				}}
+			/>
+		</>
+	);
 }
--- src/components/TasksContext.tsx
+++ src/components/TasksContext.tsx
@@ -1,97 +1,94 @@
 import { createContext, useContext, useReducer } from "react";
 
 import {
-  Task,
-  Action,
-  Tasks,
-  ChildrenProps,
-  CreateContext,
+	Task,
+	Action,
+	Tasks,
+	ChildrenProps,
+	CreateContext,
 } from "../types/TypesNConsts";
 import { v4 as uuid } from "uuid";
+import { useTasks } from "../utilities/axios";
 
 export const TasksContext = createContext<Tasks>([]);
 export const TasksDispatchContext = createContext<CreateContext | null>(null);
 
 export function TasksProvider({ children }: ChildrenProps) {
-  const [tasks, dispatch] = useReducer(tasksReducer, tasksDummies);
+	const [tasks, dispatch] = useReducer(tasksReducer, useTasks());
 
-  return (
-    <TasksContext.Provider value={tasks}>
-      <TasksDispatchContext.Provider value={dispatch}>
-        {children}
-      </TasksDispatchContext.Provider>
-    </TasksContext.Provider>
-  );
-}
-
-export function useTasks() {
-  return useContext(TasksContext);
+	return (
+		<TasksContext.Provider value={tasks}>
+			<TasksDispatchContext.Provider value={dispatch}>
+				{children}
+			</TasksDispatchContext.Provider>
+		</TasksContext.Provider>
+	);
 }
 
 export function useTasksDispatch() {
-  return useContext(TasksDispatchContext);
+	return useContext(TasksDispatchContext);
 }
 
 export const ACTIONS = {
-  ADD_TASK: "add-task",
-  CHANGE_DEADLINE: "change deadline",
-  COMPLETED: "completed",
-  CHANGE_TOPIC: "change topic",
-  CHANGE_NAME: "change name",
-  DELETE: "delete",
-  CONFIRM_CHANGE: "confirm change",
-  PRIORITY: "toggle priority",
+	ADD_TASK: "add-task",
+	CHANGE_DEADLINE: "change deadline",
+	COMPLETED: "completed",
+	CHANGE_TOPIC: "change topic",
+	CHANGE_NAME: "change name",
+	DELETE: "delete",
+	CONFIRM_CHANGE: "confirm change",
+	PRIORITY: "toggle priority",
 };
 
 function tasksReducer(tasks: Tasks, action: Action): Tasks {
-  switch (action.type) {
-    case ACTIONS.ADD_TASK:
-      return [...tasks, newTask(action.payload.userInput)];
-    case ACTIONS.CHANGE_DEADLINE:
-      return tasks.map((task) => {
-        if (task.id === action.payload.id) {
-          return { ...task, deadline: action.payload.deadline };
-        } else return task;
-      });
-    case ACTIONS.CHANGE_NAME:
-      return tasks.map((task) => {
-        if (task.id === action.payload.id) {
-          return { ...task, name: action.payload.name };
-        } else return task;
-      });
-    case ACTIONS.CHANGE_TOPIC:
-      return tasks.map((task) => {
-        if (task.id === action.payload.id) {
-          return { ...task, topic: action.payload.topic };
-        } else return task;
-      });
-    case ACTIONS.PRIORITY:
-      return tasks.map((task) => {
-        if (task.id === action.payload.id) {
-          return { ...task, priority: !task.priority };
-        } else return task;
-      });
-    case ACTIONS.COMPLETED:
-      return tasks.map((task) => {
-        if (task.id === action.payload.id) {
-          return { ...task, completed: !task.completed };
-        } else return task;
-      });
-    case ACTIONS.DELETE:
-      return tasks.filter((task) => task.id !== action.payload.id);
-    default:
-      return tasks;
-  }
+	switch (action.type) {
+		case ACTIONS.ADD_TASK:
+			return [...tasks, newTask(action.payload.userInput)];
+		case ACTIONS.CHANGE_DEADLINE:
+			return tasks.map((task) => {
+				if (task.id === action.payload.id) {
+					return { ...task, deadline: action.payload.deadline };
+				} else return task;
+			});
+		case ACTIONS.CHANGE_NAME:
+			return tasks.map((task) => {
+				if (task.id === action.payload.id) {
+					return { ...task, name: action.payload.name };
+				} else return task;
+			});
+		case ACTIONS.CHANGE_TOPIC:
+			return tasks.map((task) => {
+				if (task.id === action.payload.id) {
+					return { ...task, topic: action.payload.topic };
+				} else return task;
+			});
+		case ACTIONS.PRIORITY:
+			return tasks.map((task) => {
+				if (task.id === action.payload.id) {
+					return { ...task, priority: !task.priority };
+				} else return task;
+			});
+		case ACTIONS.COMPLETED:
+			return tasks.map((task) => {
+				if (task.id === action.payload.id) {
+					return { ...task, completed: !task.completed };
+				} else return task;
+			});
+		case ACTIONS.DELETE:
+			return tasks.filter((task) => task.id !== action.payload.id);
+		default:
+			return tasks;
+	}
 }
 
 function newTask(userInput: Task): Task {
-  return {
-    id: uuid(),
-    name: userInput.name,
-    deadline: userInput.deadline,
-    topic: userInput.topic,
-    description: userInput.description,
-    priority: userInput.priority,
-    completed: false,
-  };
+	return {
+		id: uuid(),
+		name: userInput.name,
+		deadline: userInput.deadline,
+		topic: userInput.topic,
+		description: userInput.description,
+		priority: userInput.priority,
+		completed: false,
+	};
 }
